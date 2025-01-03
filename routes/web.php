436da05e2b33413563e7a1509c3c85b28d57d870
Rodrigo Soares da Silva <guigocore@gmail.com>
2025-01-03 09:05:36 -0300

    test: add mail test class

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TaskController;
+use App\Mail\TestMailMessage;
 
 Route::get('/', function () {
     return view('welcome');
@@ -11,3 +12,7 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::resource('task', TaskController::class); // ->middleware('auth'); // Authentication middleware
+
+Route::get('message-test', function(){
+    return new TestMailMessage();
+});
